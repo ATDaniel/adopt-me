48cbd4d171558cbbcb4026080bf4fc472e058343
Drew <drew@Drews-MacBook-Pro-2.local>
2021-01-25 20:18:35 -0500

    finished tooling configs. Converted components to jsx

--- src/App.js
+++ src/App.js
@@ -3,24 +3,32 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Winston",
-      animal: "Cat",
-      breed: "American Shorthair",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "cokcatiel",
-    }),
-  ]);
+  // return React.createElement("div", {}, [
+  //   React.createElement("h1", {}, "Adopt Me"),
+  //   React.createElement(Pet, {
+  //     name: "Luna",
+  //     animal: "Dog",
+  //     breed: "havanese",
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "Winston",
+  //     animal: "Cat",
+  //     breed: "American Shorthair",
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "Pepper",
+  //     animal: "Bird",
+  //     breed: "cokcatiel",
+  //   }),
+  // ]);
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me!</h1>
+      <Pet name="Luna" animal="dog" breed="Havanese" />
+      <Pet name="Winston" animal="Cat" breed="Domestic Shorthair" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
