18adce786952bf81f7a2fb0df693743aae105b0c
Drew Daniel <DrewTDaniel@gmail.com>
2021-02-09 15:32:58 -0500

    Added effect to rerender list of breeds using useEffect. Also added API call to pet API to obtain breeds of different animals

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,16 +1,28 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
   // hooks should NEVER go inside if's and for loops
   // useState is a hook; all hooks start with "use"
   const [location, setLocation] = useState("Seattle, WA"); // [variable, changeHandler] = hook(default)
-  // const [animal, setAnimal] = useState("dog");
-  // const [breed, setBreed] = useState("");
   const [breeds, setBreeds] = useState([]);
-  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS); //Using custom Hook made in useDropdown.js
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS); //Using custom Hook from useDropdown.js
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+
+  useEffect(() => {
+    setBreeds([]); // clear Breeds list
+    setBreed(""); // clear Breed selected
+
+    pet.breeds(animal).then(({ breeds: apiBreeds }) => {
+      //Call pet.breeds() api, then map response
+      const breedStrings = apiBreeds.map(({ name }) => name); // map objects to strings
+      setBreeds(breedStrings); // Finally, set Breeds list to the list of breed strings.
+    }, console.error);
+  }, [animal, setBreed, setBreeds]); // Dependency array
+
+  // Every time there's a rerender, React checks if any of these things in the dependency array have
+  // changed. If so, the Effect is ran.
 
   return (
     <div className="search-params">
